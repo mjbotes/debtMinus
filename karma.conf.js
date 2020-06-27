24263b8a6cf3d85dda2e95f14958e5ae8f126428
Marnus Jan Botes <mbotes@student.wethinkcode.co.za>
2020-06-27 12:29:16 +0200

    update

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/OneT-Platform'),
+      dir: require('path').join(__dirname, './coverage/debtMinus'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,17 +22,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'OneT-Platform'`, () => {
+  it(`should have as title 'debtMinus'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('OneT-Platform');
+    expect(app.title).toEqual('debtMinus');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('OneT-Platform app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('debtMinus app is running!');
   });
 
   it('should render the carousel component', () => {
